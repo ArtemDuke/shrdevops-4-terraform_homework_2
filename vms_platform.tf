f3a91bdb65e283b45d8ff503edebde0884929b32
ArtemDuke <161213872+ArtemDuke@users.noreply.github.com>
2024-03-12 11:09:16 +0300

    Update vms_platform.tf

--- vms_platform.tf
+++ vms_platform.tf
@@ -22,20 +22,20 @@ variable "vm_db_platform_id" {
   description = "vm platform "
 }
 
-variable "vm_db_core" {
-  type        = number
-  default     = 2
-  description = "vm cores count "
-}
+#variable "vm_db_core" {
+#  type        = number
+#  default     = 2
+#  description = "vm cores count "
+#}
 
-variable "vm_db_memory" {
-  type        = number
-  default     = 2
-  description = "vm memory size "
-}
+#variable "vm_db_memory" {
+#  type        = number
+#  default     = 2
+#  description = "vm memory size "
+#}
 
-variable "vm_db_core_fraction" {
-  type        = number
-  default     = 5
-  description = "vm cores fraction "
-}
+#variable "vm_db_core_fraction" {
+#  type        = number
+#  default     = 5
+#  description = "vm cores fraction "
+#}
